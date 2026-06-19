031a834d800d1e251ed5f8d92053d83d40600ca4
baltha-regner <baltharegner0@gmail.com>
2026-06-19 02:28:29 -0300

    firebase modificado para que pueda sumar auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,21 @@
-// src/firebase.js
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// 1. Sumamos la importación de Autenticación
+import { getAuth } from "firebase/auth";
 
-// Estos datos te los da Firebase al crear el proyecto
 const firebaseConfig = {
-  apiKey: 'AIzaSyCif0_4qZATNzPgE20szvXLkej50PTAJE0',
-  authDomain: 'estadisticas1-1a51d.firebaseapp.com',
-  projectId: 'estadisticas1-1a51d',
-  storageBucket: 'estadisticas1-1a51d.firebasestorage.app',
-  messagingSenderId: '1021708997660',
-  appId: '1:1021708997660:web:a0655767cdae7f5c91bbd9',
-  measurementId: 'G-44TPWPBFSX',
+  apiKey: "AIzaSyCif0_4qZATNzPgE20szvXLkej50PTAJE0",
+  authDomain: "estadisticas1-1a51d.firebaseapp.com",
+  projectId: "estadisticas1-1a51d",
+  storageBucket: "estadisticas1-1a51d.firebasestorage.app",
+  messagingSenderId: "1021708997660",
+  appId: "1:1021708997660:web:a0655767cdae7f5c91bbd9"
 };
 
-// Inicializar Firebase
+// Inicializamos Firebase
 const app = initializeApp(firebaseConfig);
-// Inicializar la Base de Datos (Firestore)
+
+// Exportamos los servicios que vamos a usar
 export const db = getFirestore(app);
+// 2. Exportamos el módulo de autenticación configurado
+export const auth = getAuth(app);
